de62e602c485315dbf1b2446f05bd63e3152f59d
Xavier2414 <73123181+Xavier2414@users.noreply.github.com>
2022-02-23 17:03:45 +0100

    rooting perfils

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { RegisterComponent } from './identificacion/register/register.component'
 import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
 import { AdminHeaderComponent } from './plantillas/admin-header/admin-header.component';
+import { PerfilUsuarioComponent } from './vistas/usuario/perfil-usuario/perfil-usuario.component';
+import { PerfilAdminComponent } from './vistas/admin/perfil-admin/perfil-admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -13,6 +15,8 @@ const routes: Routes = [
   { path: 'header', component: HeaderComponent },
   { path: 'footer', component: FooterComponent },
   { path: 'admin-header', component: AdminHeaderComponent},
+  { path: 'perfil-usuario', component: PerfilUsuarioComponent},
+  { path: 'perfil-admin', component: PerfilAdminComponent}
 ];
 
 @NgModule({
---
+++ src/app/vistas/usuario/perfil-usuario/perfil-usuario.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { PerfilUsuarioComponent } from './perfil-usuario.component';
+
+describe('PerfilUsuarioComponent', () => {
+  let component: PerfilUsuarioComponent;
+  let fixture: ComponentFixture<PerfilUsuarioComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PerfilUsuarioComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PerfilUsuarioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
