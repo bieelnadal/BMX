f2f9ff52d8c302096cc5e70dc6a42e53a3f2ff5b
Xavier2414 <73123181+Xavier2414@users.noreply.github.com>
2022-03-07 17:15:27 +0100

    validacions1

--- src/app/vistas/admin/perfil-admin/perfil-admin.component.ts
+++ src/app/vistas/admin/perfil-admin/perfil-admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-perfil-admin',
@@ -8,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilAdminComponent implements OnInit {
 
-  constructor() { }
+  EditarPefil: FormGroup;
+  direciones: FormGroup;
+
+  constructor(public fb: FormBuilder, public fc: FormBuilder) {
+    this.EditarPefil = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
+      apellidos: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
+      email: ['', [Validators.required, Validators.email]],
+      DNI: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(50)]],
+    });
+     this.direciones =this.fb.group({
+       direcion: ['', [Validators.required,]],
+       codigoPostal:['',[Validators.required]],
+       Localidad:['' ,[Validators.required]],
+     });
+   }
 
   mostrarDesplegableVisual: boolean = false;
   mostrarDesplegableEditarPerfilVisual: boolean = false;
@@ -17,6 +33,14 @@ export class PerfilAdminComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  get form(){
+    return this.EditarPefil.controls;
+   }
+
+   onSubmit() {   
+    
+  }
 
 
 
