4aa73c213379c4585cb7dd7fa676f86024d01c81
Xavier2414 <73123181+Xavier2414@users.noreply.github.com>
2022-05-03 16:11:22 +0200

    adas

--- src/app/vistas/usuario/gestor-productos-vista-usuario/gestor-productos-vista-usuario.component.ts
+++ src/app/vistas/usuario/gestor-productos-vista-usuario/gestor-productos-vista-usuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/interfaces/Usuario';
+import { UsersService } from 'src/app/services/usuarios/users.service';
+import { Producto } from 'src/app/interfaces/Productos';
+import { ProductsService } from 'src/app/services/productos/products.service';
 
 @Component({
   selector: 'app-gestor-productos-vista-usuario',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestor-productos-vista-usuario.component.css']
 })
 export class GestorProductosVistaUsuarioComponent implements OnInit {
+  datosProducto: Producto = {
+    idProducto: 0,
+    idVendedor: 0,
+    Nombre: '',
+    Imagen: '',
+    Descripcion: '',
+    idCategoria: 0,
+    Fecha: '',
+    Estado: 0,
+    Activo: 0,
+    Precio: 0,
+    Subasta: 0,
+  };
 
-  constructor() { }
+  listaProductos: any[] = [];
+  producto: any;
+
+  constructor(
+    private userServ: UsersService,
+    private prodServ: ProductsService
+  ) {}
 
   ngOnInit() {
+    this.obtenerProductos();
   }
 
+  obtenerProductos() {
+    this.listaProductos = [];
+    this.prodServ.obtenerProductosAdmin().subscribe((val: any) => {
+      this.producto = val;
+      if (this.producto == null) {
+      } else {
+
+        val.forEach((element: any) => {
+          
+          this.listaProductos.push(element);
+        });
+      }
+    });
+  }
 }
