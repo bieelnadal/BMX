610cff7f84c5913fc1ca7ab8d880f3be1a60325d
Xavier2414 <73123181+Xavier2414@users.noreply.github.com>
2022-05-06 17:33:12 +0200

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ import { SobreNosotrosComponent } from './plantillas/sobre-Nosotros/sobre-Nosotr
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'header', component: HeaderComponent, canActivate:[GuardService] },
